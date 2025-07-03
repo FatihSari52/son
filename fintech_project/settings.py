e012765c5bac5267aa587409b1916ec0073f9b7d
FatihSari52 <162148177+FatihSari52@users.noreply.github.com>
2025-07-03 17:21:46 +0300

    Update settings.py

--- fintech_project/settings.py
+++ fintech_project/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(conn_max_age=600)
+        'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
     }
 }
 
